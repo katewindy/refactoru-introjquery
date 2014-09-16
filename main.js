d40b1ba4d3afcb25d6b468781e652663a74b85da
Kate Windheuser <roboticbrain@gmail.com>
2014-09-16 17:47:44 -0600

    end of day push

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-$(document).on('ready', function() {
-  
-});
+// click event handler that prints a message to the console.
+
+// $(document).ready(function() {
+// 	$('.button').click(function(){
+// 		console.log("Stop clicking me!");
+// 	});
+// });
+
+// click event handler that appends some text, an unordered list and an h1 with text to the body element.
+
+// $(document).ready(function() {
+// 	$('.button1').click(function(){
+// 		$('body').append("<p>Here's some text!</p>");
+// 	});
+// 	$('.button2').click(function(){
+// 		$('body').append("<h1>I'm a title!</h1>");
+// 	});
+// 	$('.button3').click(function(){
+// 		$('body').append("<ul><li>item1</li><li>item2</li><li>item3</li></ul>");
+// 	});
+// });

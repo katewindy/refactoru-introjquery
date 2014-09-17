e203b0305d2b4f1c703e9a0f2d1bc773dc823980
Kate Windheuser <roboticbrain@gmail.com>
2014-09-17 14:02:05 -0600

    started on section III, giant buttons

--- main.js
+++ main.js
@@ -20,22 +20,45 @@
 // 	});
 // });
 
-$(document).ready(function(){
-	$('p').hover(function (){
-		$(this).css("color", "pink");
-	}, function(){
-		$(this).css("color", "#80809A");
+
+// JS for Part II: 
+// paragraphs become pink when hovered over, all headers get a "!" appended,
+// and links popup a confirmation box.  If you cancel out of the confirmation box
+// the link is not opened and also the link disappears from the page.
+// 
+// $(document).ready(function(){
+// 	$('p').hover(function (){
+// 		$(this).css("color", "pink");
+// 	}, function(){
+// 		$(this).css("color", "#80809A");
+// 	});
+// 	$('h1, h2, h3').append("!");
+// 	$('a').click(function(){
+// 		var letsGo = confirm("Do you want to visit this link?");
+// 		if (letsGo) {
+
+// 		}
+// 		else {
+// 			$(this).remove();
+// 			return false;
+
+// 		}
+// 	})
+// });
+
+
+// GIANT BUTTONS JS
+// 
+// 
+
+$(document).ready(function() {
+
+	$('.button').click(function(){
+		$('body').append('<div class="lightbox-overlay"><div class="lightbox-content">
+			<h3>I\'m a Popup\!</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing 
+			elit. Necessitatibus, perferendis amet in! Odio cum numquam, officiis expedita 
+			ex architecto consectetur molestiae neque enim sed magnam sapiente doloremque 
+			earum asperiores veritatis!</p></div></div>');
 	});
-	$('h1, h2, h3').append("!");
-	$('a').click(function(){
-		var letsGo = confirm("Do you want to visit this link?");
-		if (letsGo) {
 
-		}
-		else {
-			$(this).remove();
-			return false;
-
-		}
-	})
 });
